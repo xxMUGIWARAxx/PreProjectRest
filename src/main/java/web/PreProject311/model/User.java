5d38ec1c66f2352ae7fe48c033a9dcd9959d602a
xxMUGIWARAxx <misha_grigoryan_83@mail.ru>
2025-08-03 16:52:19 +0300

    rest commit 1

--- src/main/java/web/PreProject311/model/User.java
+++ src/main/java/web/PreProject311/model/User.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 
 @Entity
-@Table(name = "User")
+@Table(name = "users")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,7 +23,7 @@ public class User implements UserDetails {
 
     private String password;
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
